1623fc7f38d4e1316b21bff17ea84fdde5b63e04
Evertton Cardoso <nova.ordem.60@gmail.com>
2023-07-04 09:22:35 -0300

    atualizações

--- scripty.js
+++ scripty.js
@@ -8,33 +8,35 @@ function colocarNaTela(dados) {
   document.querySelector(".descricao").innerHTML = dados.weather[0].description;
   document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
   document.querySelector(".icone").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+
+  const cidade = document.querySelector(".input-cidade").value;
+  buscarImagem(cidade);
 }
 
-async function buscarCidade(cidade) {
+async function buscarClima(cidade) {
   try {
-    const dadosResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`);
-    const dados = await dadosResponse.json();
+    const urlClima = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`;
+    const responseClima = await fetch(urlClima);
+    const dataClima = await responseClima.json();
 
-    if (dadosResponse.ok) {
-      colocarNaTela(dados);
-      buscarImagem(cidade);
+    if (responseClima.ok) {
+      colocarNaTela(dataClima);
     } else {
-      console.log("Erro ao buscar dados do clima:", dados);
+      console.log("Erro ao buscar dados do clima:", dataClima);
     }
   } catch (error) {
-    console.log("Erro na requisição:", error);
+    console.log("Erro na requisição do clima:", error);
   }
 }
 
 async function buscarImagem(cidade) {
-  const url = `https://api.unsplash.com/photos/random?query=${cidade}&orientation=landscape&client_id=${accessKey}&fit=crop&w=1920&h=1080`;
-
   try {
-    const response = await fetch(url);
-    const data = await response.json();
+    const urlImagem = `https://api.unsplash.com/photos/random?query=${cidade}&orientation=landscape&client_id=${accessKey}&fit=crop&w=800&h=600`;
+    const responseImagem = await fetch(urlImagem);
+    const dataImagem = await responseImagem.json();
 
-    if (response.ok) {
-      const imagemUrl = data.urls.raw;
+    if (responseImagem.ok) {
+      const imagemUrl = dataImagem.urls.raw;
       const imagem = new Image();
       imagem.src = imagemUrl;
       imagem.onload = function () {
@@ -44,10 +46,10 @@ async function buscarImagem(cidade) {
         document.body.style.backgroundPosition = "center";
       };
     } else {
-      console.log("Erro ao buscar imagem:", data.errors);
+      console.log("Erro ao buscar imagem:", dataImagem.errors);
     }
   } catch (error) {
-    console.log("Erro na requisição:", error);
+    console.log("Erro na requisição da imagem:", error);
   }
 }
 
@@ -60,5 +62,9 @@ document.querySelector(".input-cidade").addEventListener("keydown", function (ev
 
 function cliqueiNoBotao() {
   let cidade = document.querySelector(".input-cidade").value;
-  buscarCidade(cidade);
+  buscarClima(cidade);
 }
+
+// Exemplo de uso
+const cidade = "São Paulo";
+buscarClima(cidade);
